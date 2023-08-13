586f2d04b7f7b03ac2288b41c3ee07d4716c5738
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-08-14 00:39:32 +0900

    getstat WAs

--- getstat.jl
+++ getstat.jl
@@ -1,27 +1,48 @@
 using StatsBase
 
+function print_stat(vec)
+    println("Sum = $(sum(vec))")
+    println("Mean = $(mean(vec))")
+    println("Median = $(median(vec))")
+    println("Min = $(minimum(vec))")
+    println("Max = $(maximum(vec))")
+end
+
 function main()
     errdir = "err"
 
-    Stats = map(
+    Scores = map(
         file -> begin
             for line in readlines("$errdir/$file")
-                if startswith(line, "Score")
+                if startswith(line, "Score = ")
                     return (file, parse(Int, line[9:end]))
                 end
             end
             println(stderr, "Score is not calculated on $file.")
         end, readdir(errdir)
     )
-    filter!(x -> !isnothing(x), Stats)
+    filter!(x -> !isnothing(x), Scores)
 
-    Scores = map(x -> x[2], Stats)
+    WAs = map(
+        file -> begin
+            for line in readlines("$errdir/$file")
+                if startswith(line, "Number of wrong answers = ")
+                    return (file, parse(Int, line[27:end]))
+                end
+            end
+            println(stderr, "Score is not calculated on $file.")
+        end, readdir(errdir)
+    )
+    filter!(x -> !isnothing(x), WAs)
+
+    Scores = map(x -> x[2], Scores)
+    WAs = map(x -> x[2], WAs)
+
     println("Testcase number = $(length(Scores))")
-    println("Sum = $(sum(Scores))")
-    println("Mean = $(mean(Scores))")
-    println("Median = $(median(Scores))")
-    println("Min = $(minimum(Scores))")
-    println("Max = $(maximum(Scores))")
+    println("Stats of Scores: ")
+    print_stat(Scores)
+    println("Stats of WAs: ")
+    print_stat(WAs)
 end
 
 main()
