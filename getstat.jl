912697f44e1879e2066123b8e0822ec0ffea1d4e
Capobmb <82255007+Capobmb@users.noreply.github.com>
2023-08-23 21:54:32 +0900

    compare

--- getstat.jl
+++ getstat.jl
@@ -1,7 +1,7 @@
 using DataFrames
 
 function main()
-    errdir = "err"
+    errdir = "err_dir"
 
     df = DataFrame(file=String[],
         Score=Int[], n_WA=Int[], PlacementCost=Int[], MeasurementCost=Int[], MeasurementCount=Int[])
@@ -25,6 +25,52 @@ function main()
     end
 
     println(describe(df))
+end
+
+function compare()
+    indir = "in"
+    errdir1 = "err"
+    errdir2 = "err_dir"
+
+    df = DataFrame(L=Int[],N=Int[],S=Int[],density=Float64[],bef=Int[],aft=Int[])
+
+    for file in readdir(indir)
+        L, N, S = parse.(Int,split(readlines("$indir/$file")[1]))
+
+        score1 = -1
+        for line in readlines("$errdir1/$file")
+            if startswith(line, "Score")
+                score1 = parse(Int, line[length("Score")+4:end]) # " = " の後
+                break
+            end
+        end
+
+        score2 = -1
+        for line in readlines("$errdir2/$file")
+            if startswith(line, "Score")
+                score2 = parse(Int, line[length("Score")+4:end]) # " = " の後
+                break
+            end
+        end
+
+        push!(df, [L, N, S, N / L * L, score1, score2])
+    end
+
+    increased = filter(x -> x.bef < x.aft, df)
+    decreased = filter(x -> x.bef >= x.aft, df)
+
+    println(describe(increased))
+    println(describe(decreased))
+
+    # increased = filter(x -> Score1[x] < Score2[x], eachindex(Score1))
+    # decreased = filter(x -> Score1[x] >= Score2[x], eachindex(Score1))
+    # for i in increased
+    #     println(LNS[i])
+    # end
+    # println("----")
+    # for i in decreased
+    #     println(LNS[i])
+    # end
 end
 
 main()
